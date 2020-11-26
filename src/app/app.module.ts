77a80e166de82c60224c280bd8339ac28af646f9
Helton Oliveira <helton.bjj@gmail.com>
2020-11-26 01:05:47 -0500

    implementing get Customers from backend - INITIAL COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,42 @@
+import { DecimalPipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { JwtHelperService, JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TextMaskModule } from 'angular2-text-mask';
+import { NgBrazil } from 'ng-brazil';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuardService } from './auth/auth-guard.service';
+import { SortableDirective } from './directives/sortable.directive';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { LoginComponent } from './pages/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CustomerComponent
+    CustomerComponent,
+    SortableDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule, 
     HttpClientModule,
-    JwtModule
+    JwtModule,
+    NgbModule,
+    FormsModule,
+    TextMaskModule,
+    NgBrazil
   ],
   providers: [
     AuthGuardService,
-    {provide: JWT_OPTIONS, useValue: JWT_OPTIONS}, JwtHelperService
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS }, JwtHelperService,
+    DecimalPipe
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-customer',
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
+  
+  items$: Observable<any[]>;
+  filter = new FormControl( '' );
 
-  constructor() { }
+  private items: any[] | any;
+  private token: string = sessionStorage.getItem( 'token' );
 
-  ngOnInit() {
+  constructor (
+    private pipe: DecimalPipe,
+    private apiService: ApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit () {
+    setTimeout(() => {
+      this.apiService
+        .getAllCustomers()
+        .subscribe( response  => { 
+          this.items = response;
+        }, err => { 
+            console.error('[Customers] => ',  err.message);
+        })
+      this.items$ = this.filter.valueChanges.pipe(
+        startWith( '' ),
+        map( text => this.search( text, this.pipe ) )
+      );
+    }, 1000);
+  }
+
+  private search ( text: string, pipe: PipeTransform ): any[] {
+    if (this.items) {
+      return this.items.filter( item => {
+        const term = text.toLowerCase();
+        return item.name.toLowerCase().includes( term )
+          || pipe.transform( item.cpf ).includes( term )
+          || pipe.transform( item.zipcode ).includes( term );
+      } );
+    }
+  }
+
+
+
+  logout () {
+    sessionStorage.removeItem( 'token' );
+    this.router.navigateByUrl( '/' );
   }
 
 }
